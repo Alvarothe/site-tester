2c70a0eba09955449e83a2a990f5627a7cfb9667
Alvaro L. <95324348+Alvarothe@users.noreply.github.com>
2024-09-28 18:14:17 -0300

    Update script_g.js

--- script_g.js
+++ script_g.js
@@ -229,7 +229,7 @@ style.textContent = `
 
 @keyframes blink-yellow {
     0% { background-color: transparent; }
-    50% { background-color: rgba(255, 255, 0, 0.9); }
+    50% { background-color: rgba(255, 255, 0, 0.3); }
     100% { background-color: transparent; }
 }
 `;
